79a08760ecb57e5522d65705e6feb722edd76cc1
sinchanakn17 <142564079+sinchanakn17@users.noreply.github.com>
2023-09-05 20:54:26 +0530

    Add files via upload

---
+++ chess board.py
@@ -0,0 +1,22 @@
+import turtle
+p=turtle.Turtle()
+def square():
+    for i in range (4):
+    p.forward(40)
+    p.right(90)
+    p.forward(40)
+    for i in range (8)
+    p.penup()
+    p.goto(0,40*i)
+    p.pendown()
+    for x in range (8)
+    if (x,+i)%2==0
+    color = "Black"
+    else:
+        color = "White"
+        p.fillcolor(color)
+        p.begin_fill()
+        square()
+        p.end_fill()
+    
+    
